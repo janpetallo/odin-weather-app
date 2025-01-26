52167c5670dca65f2da364d0857ee1188536095e
janpetallo <101074771+janpetallo@users.noreply.github.com>
2025-01-26 15:44:12 -0700

    fetched and console logged weather data

--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@ import "./scss/styles.scss";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+
+
+async function fetchWeatherData() {
+
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/calgary?unitGroup=us&key=MKKNHQDK4MYP85WWXKVT6FV2V&contentType=json`);
+    const data = await response.json();
+    console.log(data);
+
+}
+
+fetchWeatherData();
+
+
+
+
+
+
+
+
