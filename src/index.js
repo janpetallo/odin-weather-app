36fc68c7faa93190fddba8344501592db4f7c399
janpetallo <101074771+janpetallo@users.noreply.github.com>
2025-01-26 19:40:22 -0700

    added mode cors to fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Import our custom CSS
 import "./scss/styles.scss";
+import "./styles.css";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
@@ -10,7 +11,9 @@ import { displayWeeklyForecast } from "./modules/weekly";
 
 
 async function fetchWeatherData() {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/calgary?unitGroup=us&key=MKKNHQDK4MYP85WWXKVT6FV2V&contentType=json`);
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/calgary?unitGroup=us&key=MKKNHQDK4MYP85WWXKVT6FV2V&contentType=json`,
+        { mode: 'cors' }
+    );
     const data = await response.json();
     console.log(data);
     
