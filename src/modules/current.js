1c4ed365276e9d1c14fdc14f0bc78d4cc1c30d9b
janpetallo <101074771+janpetallo@users.noreply.github.com>
2025-01-27 19:24:56 -0700

    Structured current conditions section

--- src/modules/current.js
+++ src/modules/current.js
@@ -1,8 +1,10 @@
 const cityElement = document.querySelector('#currentConditions_city');
 const countryElement = document.querySelector('#currentConditions_country');
 const temperatureElement = document.querySelector('#currentConditions_temperature');
+const highLowElement = document.querySelector('#highLowValue');
 const descriptionElement = document.querySelector('#currentConditions_description');
 const feelsLikeElement = document.querySelector('#feelsLikeValue');
+const precipitationElement = document.querySelector('#precipitationValue');
 const humidityElement = document.querySelector('#humidityValue');
 const windElement = document.querySelector('#windValue');
 
@@ -11,6 +13,9 @@ function displayCurrentConditions(data) {
     const [city, stateProvince, country] = data.resolvedAddress.split(',');
     const currentTemp = currentConditions.temp;
     const feelsLike = currentConditions.feelslike;
+    const precipitation = currentConditions.precipprob;
+    const high = data.days[0].tempmax;
+    const low = data.days[0].tempmin;
     const conditions = currentConditions.conditions;
     const humidity = currentConditions.humidity;
     const wind = currentConditions.windspeed;
@@ -18,8 +23,10 @@ function displayCurrentConditions(data) {
     cityElement.textContent = city;
     countryElement.textContent = country;
     temperatureElement.textContent = currentTemp;
+    highLowElement.textContent = `H: ${high} / L: ${low}`;
     descriptionElement.textContent = conditions;
     feelsLikeElement.textContent = feelsLike;
+    precipitationElement.textContent = `${precipitation}%`;
     humidityElement.textContent = humidity;
     windElement.textContent = wind;
 }
