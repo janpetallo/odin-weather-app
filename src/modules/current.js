6569bd24ab58a59c47ec9bb98c88e153133e9828
janpetallo <101074771+janpetallo@users.noreply.github.com>
2025-01-27 18:27:33 -0700

    removed css grid

--- src/modules/current.js
+++ src/modules/current.js
@@ -2,9 +2,9 @@ const cityElement = document.querySelector('#currentConditions_city');
 const countryElement = document.querySelector('#currentConditions_country');
 const temperatureElement = document.querySelector('#currentConditions_temperature');
 const descriptionElement = document.querySelector('#currentConditions_description');
-const feelsLikeElement = document.querySelector('#currentConditions_feelsLike');
-const humidityElement = document.querySelector('#currentConditions_humidity');
-const windElement = document.querySelector('#currentConditions_wind');
+const feelsLikeElement = document.querySelector('#feelsLikeValue');
+const humidityElement = document.querySelector('#humidityValue');
+const windElement = document.querySelector('#windValue');
 
 function displayCurrentConditions(data) {
     const currentConditions = data.currentConditions;
@@ -13,7 +13,7 @@ function displayCurrentConditions(data) {
     const feelsLike = currentConditions.feelslike;
     const conditions = currentConditions.conditions;
     const humidity = currentConditions.humidity;
-    const wind = currentConditions.wind;
+    const wind = currentConditions.windspeed;
 
     cityElement.textContent = city;
     countryElement.textContent = country;
